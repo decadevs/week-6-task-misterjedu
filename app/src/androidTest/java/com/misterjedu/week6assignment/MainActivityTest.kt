997cf70a33cc6c81328e71d15b6b6c9efd5a9fb3
Oladokun Oladapo <oladokun104@gmail.com>
2020-08-26 16:21:27 +0100

    More Tests added

--- app/src/androidTest/java/com/misterjedu/week6assignment/MainActivityTest.kt
+++ app/src/androidTest/java/com/misterjedu/week6assignment/MainActivityTest.kt
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 
 class MainActivityTest {
-    //Declare Variables
+    //Declare Global Variables
     private var fullName = "Oladokun Oladapo"
     private  var phoneNumnber = "07013473097"
     private var emailAddress = "oladokun104@gmail.com"
@@ -30,47 +30,71 @@ class MainActivityTest {
     @get: Rule
     val activityRule : ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
 
-
+    //Test if main activity is on view
     @Test
     fun test_main_activity_in_view() {
         onView(withId(R.id.main_activity)).check(matches(isDisplayed()))
     }
 
+    //Test if main activity background image and text are on view
     @Test
     fun test_main_activity_background() {
         onView(withId(R.id.main_activity_background_image)).check(matches(isDisplayed()))
         onView(withId(R.id.main_activity_background_text)).check(matches(isDisplayed()))
     }
 
+    //Test if main background text display correct views
     @Test
     fun is_main_activity_background_text_displayed() {
         onView(withId(R.id.main_activity_background_text)).check(matches(withText(R.string.sign_up_page)))
     }
 
+    //Test if username icon and user input text view is displayed
     @Test
     fun test_main_activity_user_name() {
         onView(withId(R.id.username_icon)).check(matches(isDisplayed()))
         onView(withId(R.id.full_name_input)).check(matches(isDisplayed()))
     }
 
+    //Test if user name text view hint is correct
+    @Test
+    fun test_main_activity_user_name_hint(){
+        onView(withId(R.id.full_name_input)).check(matches(withHint(R.string.name_hint)))
+    }
+
+    //Test if phone icon and phone number textview are displayed
     @Test
     fun test_main_activity_phone_number() {
         onView(withId(R.id.phone_icon)).check(matches(isDisplayed()))
         onView(withId(R.id.phone_number_input)).check(matches(isDisplayed()))
     }
 
+    //Test if phone number text view hint is correct
     @Test
-    fun test_main_activity_email_address() {
+    fun test_main_activity_phone_number_hint(){
+        onView(withId(R.id.phone_number_input)).check(matches(withHint(R.string.number_hint)))
+    }
+
+    //Test if email address icon and text view are displayed
+    @Test
+    fun test_main_activity_user_email_address() {
         onView(withId(R.id.email_icon)).check(matches(isDisplayed()))
         onView(withId(R.id.email_address_input)).check(matches(isDisplayed()))
     }
+    //Test if email address text view hint is accurate
+    @Test
+    fun test_main_activity_user_email_hint(){
+        onView(withId(R.id.email_address_input)).check(matches(withHint(R.string.email_hint)))
+    }
 
+    //Test if gender icon and gender spinner views are displayed
     @Test
     fun test_main_activity_gender() {
         onView(withId(R.id.gender_icon)).check(matches(isDisplayed()))
         onView(withId(R.id.gender_select)).check(matches(isDisplayed()))
     }
 
+    //Test if terms and agreement checkbox and select views are displayed
     @Test
     fun test_main_activity_terms_agreement() {
         onView(withId(R.id.terms_check_box)).check(matches(isDisplayed()))
@@ -78,9 +102,16 @@ class MainActivityTest {
         onView(withId(R.id.terms_text)).check(matches(withText(R.string.terms_conditions)))
     }
 
+    //Test if sign-up button is displayed
     @Test
     fun test_main_activity_sign_up_button() {
         onView(withId(R.id.sign_up_button)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    //Test if sign-up button text is correct
+    @Test
+    fun test_main_activity_sign_up_button_test(){
+        onView(withId(R.id.sign_up_button)).check(matches(withText(R.string.sign_up)))
     }
 
 
